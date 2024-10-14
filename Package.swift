9a2da69ef5fa53abb02886f7307f453bead17d7c
Alexey <khimuninaa@gmail.com>
2024-10-14 11:58:46 +0300

    1.0.0

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EDNAPushXCE",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "EDNAPushXCE",
+            type: .dynamic,
+            targets: ["EDNAPushXCE"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "EDNAPushXCE",
+            url: "https://github.com/edna-io/push-x-ce-ios/releases/download/1.3.0/edna-push-xce-1.3.0.zip",
+            checksum: "2a5582577b3704522c659e894592b35feebfa51b2c7d828c5420b3465e89fc3a"
+        ),
+    ]
+)
