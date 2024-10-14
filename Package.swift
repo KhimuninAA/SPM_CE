b1c36a370b5a485b5b9fb3ef720341027b9f787d
Alexey <khimuninaa@gmail.com>
2024-10-14 17:41:54 +0300

    1.0.7

--- Package.swift
+++ Package.swift
@@ -12,17 +12,22 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EDNAPushXCE",
-            targets: ["_CEStub", "EDNAPushXCE"]),
+            targets: ["_CEStub"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "_CEStub"),
+        .target(
+            name: "_CEStub",
+            dependencies: [
+                .target(name: "EDNAPushXCE")
+            ]
+        ),
         .binaryTarget(
             name: "EDNAPushXCE",
-            url: "https://github.com/KhimuninAA/SPM_CE/releases/download/1.0.6/edna-push-xce-1.3.0.zip",
+            url: "https://github.com/KhimuninAA/SPM_CE/releases/download/1.0.7/edna-push-xce-1.3.0.zip",
             checksum: "2a5582577b3704522c659e894592b35feebfa51b2c7d828c5420b3465e89fc3a"
         ),
     ]
